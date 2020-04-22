f8c93583b45aa5408a21d1b2b6ad4926934a7a09
pshoultz <pshoultzula@gmail.com>
2020-04-22 10:44:35 -0500

    had to create a new angular client because the old one fucked out somehow

--- apps/web-app/client-v2/src/app/app.module.ts
+++ apps/web-app/client-v2/src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AdminModule } from './admin/admin.module';
-import { PublicModule } from './public/public.module';
-//import { MaterialModule } from './material/material.module';
+import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material';
+
+import{ MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -17,12 +14,8 @@ import { MatButtonModule } from '@angular/material';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    AdminModule,
-    PublicModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    //MaterialModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- apps/web-app/client/src/app/material/material.module.ts
+++ apps/web-app/client/src/app/material/material.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import{ MatButtonModule } from '@angular/material';
+
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatButtonModule
+  ],
+  exports: [ 
+    MatButtonModule
+  ],
 })
 export class MaterialModule { }
